ef2056dfda4ea7c9174de44b79e84b7e5c0d6e21
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-10-16 20:27:40 +0200

    Update signup form layout to hold state

--- apps/web/app/auth/signup/signupForm.tsx
+++ apps/web/app/auth/signup/signupForm.tsx
@@ -3,12 +3,15 @@
 import React from 'react'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import { useActionState } from 'react'
+import { signUp } from '@/lib/auth'
 import SubmitButton from '@/components/ui/submitButton'
 
 const SignUpForm = () => {
+    const [state, action] = useActionState(signUp, undefined);
 
     return (
-        <form>
+        <form action={action}>
         <div className="flex flex-col gap-2">
             
         <div>
@@ -16,10 +19,35 @@ const SignUpForm = () => {
             <Input id="username" name="username" placeholder="john_doe" />
         </div>
 
+        {state?.error?.username && (
+            <div className="text-sm text-red-500">
+                <p>Username must:</p>
+                <ul>
+                {state.error.username.map((error) => (
+                    <li key={error}>{error}</li>
+                ))}
+                </ul>
+            </div>
+            )}
+
         <div>
             <Label htmlFor="password">Password</Label>
             <Input id="password" name="password" type="password" />
         </div>
+        {state?.error?.password && (
+            <div className="text-sm text-red-500">
+                <p>Password must:</p>
+                <ul>
+                {state.error.password.map((error) => (
+                    <li key={error}>{error}</li>
+                ))}
+                </ul>
+            </div>
+            )}
+        
+        {state?.message && (
+          <p className="text-sm text-red-500 justify-center">{state.message}</p>
+        )}
 
         <SubmitButton>Sign Up</SubmitButton>
         
