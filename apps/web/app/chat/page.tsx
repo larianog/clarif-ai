d8e18324e357c388fa4690d14856779c793650e8
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-10-20 02:37:43 +0200

    Add upload document button in chat page

--- apps/web/app/chat/page.tsx
+++ apps/web/app/chat/page.tsx
@@ -1,55 +1,26 @@
 "use client"
 
-import React, { useState } from "react";
-import { IconPlus } from "@tabler/icons-react"
-import { ArrowUpIcon } from "lucide-react"
+import React from "react";
 
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupButton,
-  InputGroupTextarea,
-} from "@/components/ui/input-group"
 import ChatAI from "./chat";
+import { Button } from "@/components/ui/button";
 
 const ChatPage = () => {
-    const [message, setMessage] = useState("");
-    
   return (
-    <ChatAI/> /*
     <div className = "h-screen flex items-center justify-center flex-col gap-10 container mx-auto">
      <h1 className="text-center text-2xl font-bold mb-4">
-        Chat Page
+      ClarifAI
       </h1>
 
-    <div className="grid w-full max-w-sm gap-6">
+      <Button variant="outline" aria-label="Submit">
+        Upload a document
+      </Button>
 
-      <InputGroup>
-        <InputGroupTextarea placeholder="Ask your question" />
-        <InputGroupAddon align="block-end">
-          <InputGroupButton
-            variant="outline"
-            className="rounded-full"
-            size="icon-xs"
-          >
-            <IconPlus />
-          </InputGroupButton>
-
-          <InputGroupButton
-            variant="default"
-            className="rounded-full"
-            size="icon-xs"
-          >
-            <ArrowUpIcon />
-            <span className="sr-only">Send</span>
-          </InputGroupButton>
-        </InputGroupAddon>
-      </InputGroup>
-      
-    </div>
+      <ChatAI/>
+    
     <hr />
 
-</div>*/
+</div>
   );
 };
 
