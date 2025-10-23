e5faaf3b7bfe66178de5a3c33921f4f8e02b4d32
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-10-24 00:05:41 +0200

    Enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,17 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: '*' ,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'User-Agent',
-    ],
+    credentials: true,
   });
-
 
   await app.listen(process.env.PORT ?? 8000);
   app.useGlobalPipes(
